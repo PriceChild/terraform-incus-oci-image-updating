939bf83743823da1b8f01b5a9bf5567d271c6b5c
Joseph Price <joe@pricey.uk>
2025-12-03 21:28:11 +0000

    create_before_destroy causing fingerprint drift?

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ resource "incus_image" "image" {
   }
 
   lifecycle {
-    create_before_destroy = true
       postcondition {
         condition     = self.fingerprint == data.external.image.result.fingerprint
         error_message = "Image alias does not match source fingerprint. Not sure why... best taint it."
