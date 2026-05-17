9c4130c5834c74f736b97353d8c217631d0f34d8
Nguyen Thai <39090621+tk-nguyen@users.noreply.github.com>
2026-05-17 23:44:56 +0700

    Correctly use the reference

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "incus_image" "image" {
   source_image  = {
     remote = var.docker_remote
-    name = var.docker_image
+    name ="${var.docker_image}@sha256:${data.docker_registry_image.image.sha256_digest}"
   }
   project = var.incus_project
   alias {
