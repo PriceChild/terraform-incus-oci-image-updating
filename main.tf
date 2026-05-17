895907cc72792bb5f729f0c5e41765a9a21d7f6f
Nguyen Thai <39090621+tk-nguyen@users.noreply.github.com>
2026-05-17 23:47:02 +0700

    Removed `sha256:` prefixes

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "incus_image" "image" {
   source_image  = {
     remote = var.docker_remote
-    name ="${var.docker_image}@sha256:${data.docker_registry_image.image.sha256_digest}"
+    name ="${var.docker_image}@${data.docker_registry_image.image.sha256_digest}"
   }
   project = var.incus_project
   alias {
-    name = data.docker_registry_image.image.sha256_digest
+    name = trimprefix(data.docker_registry_image.image.sha256_digest, "sha256:")
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "fingerprint" {
   description = "SHA256 digest of the provided docker image"
 }
 output "docker_image" {
-  value = "${var.docker_image}@sha256:${data.docker_registry_image.image.sha256_digest}"
+  value = "${var.docker_image}@${data.docker_registry_image.image.sha256_digest}"
   description = "Docker image reference with digest"
 }
